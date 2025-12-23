af261ac8a6810c9011aeb4cd0c7ebfa650eedbb8
hayate <hayate.0587@gmail.com>
2025-12-23 16:39:49 +0900

    Success

--- test_calc.py
+++ test_calc.py
@@ -1,7 +1,4 @@
 from calc import add
 
 def test_add():
-    assert add(1, 2) == 999, 'add関数の計算結果が不正'
-
-def test_add_success():
-    assert add(1, 2) == 3
+    assert add(1, 2) == 3, 'add関数の計算結果が不正'
